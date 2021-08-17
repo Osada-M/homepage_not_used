0e9750e0a64312f9a19e50c0a76ebe8af556e9ae
Osada-M <76993392+Osada-M@users.noreply.github.com>
2021-08-17 13:21:02 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 // console.log(window.innerWidth);
 
-function dl( f ){
-    if(window.confirm('zipファイルをダウンロードします．よろしいですか？')){
+function dl(f){
+    if(window.confirm("zipファイルをダウンロードします．よろしいですか？")){
         location.href = f
     }
 }
 
-function jump( f ){
-    let fs = 'リンク先 : ' + f + 'に飛ぼうとしています．よろしいですか？'
+function jump(f){
+    let fs = "リンク先 : " + f + "に飛ぼうとしています．よろしいですか？"
     if(window.confirm(fs)){
         location.href = f
     }
 }
 
-function back( f ){
-    if(window.confirm('戻ります．よろしいですか？')){
+function back(f){
+    if(window.confirm("戻ります．よろしいですか？")){
         location.href = f
     }
 }
